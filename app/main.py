736cbe479613740099c85b10f17ff65813fca235
Yuliia Zahoruulko <yzahorulko@gmail.com>
2025-12-30 21:15:50 +0200

    rewrite if-else

--- app/main.py
+++ app/main.py
@@ -12,10 +12,9 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other: Distance | int | float) -> Distance:
-        if isinstance(other, Distance):
-            return Distance(self.km + other.km)
-        else:
-            return Distance(self.km + other)
+        return Distance(self.km + other.km) \
+            if isinstance(other, Distance) \
+            else Distance(self.km + other)
 
     def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
@@ -31,10 +30,9 @@ class Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: Distance | int | float) -> bool:
-        if isinstance(other, Distance):
-            return self.km < other.km
-        else:
-            return self.km < other
+        return self.km < other.km \
+            if isinstance(other, Distance) \
+            else self.km < other
 
     def __gt__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
